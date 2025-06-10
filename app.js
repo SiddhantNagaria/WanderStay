5f092d0a01cc2e07bde54340e94459160e486924
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 23:23:01 +0530

    Edit and Update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 const app = express();
+const methodOverride = require("method-override");
 
 async function connectDB() {
     await mongoose.connect("mongodb://localhost:27017/WanderStay");
@@ -18,6 +19,7 @@ connectDB()
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 
 app.get("/", (req, res) => {
@@ -52,6 +54,20 @@ app.get("/listings/:id", async (req, res) => {
     res.render("listings/show.ejs", {listing});
 })
 
+//edit
+app.get("/listings/:id/edit", async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+})
+
+
+//update route
+app.put("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
 
 // app.get("/testListing", async(req,res)=>{
 //     let sampleListing = new Listing({
