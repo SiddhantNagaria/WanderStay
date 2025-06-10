9311b36a5030e563fc005444eb1a6585b8dc4938
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 22:45:54 +0530

    show route

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ connectDB()
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
@@ -29,6 +30,15 @@ app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
 })
+
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+})
+
 
 // app.get("/testListing", async(req,res)=>{
 //     let sampleListing = new Listing({
