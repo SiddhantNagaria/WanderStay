e81f35918380b67e5260d634b238a981012d82de
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 23:43:51 +0530

    layouts boilerplate

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Listing = require("./models/listing");
 const path = require("path");
 const app = express();
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 async function connectDB() {
     await mongoose.connect("mongodb://localhost:27017/WanderStay");
@@ -20,7 +21,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
