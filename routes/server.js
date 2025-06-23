26f58032e725fa10598728a0556b3bcd135d3999
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-23 23:55:53 +0530

    cookie-parser

--- routes/server.js
+++ routes/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const users = require("./user");
 const posts = require("./post")
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hi i am root");
@@ -15,6 +18,16 @@ app.get("/getcookies", (req, res) => {
     res.cookie("greet", "hello");
     res.cookie("hello", "world");
     res.send("cookies attached");
+});
+
+app.get("/", (req, res) => {
+    console.dir(req.cookies);
+    res.send("hi i am root");
+})
+
+app.get("/greet", (req, res) => {
+    let { name = "anonymous" } = req.cookies;
+    res.send(`hi ${name}`);
 })
 
 app.listen(8080, (req, res) => {
